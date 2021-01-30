30a04b3a4009a87cdfb732a7a86f4e458e883fed
AlanGarc1a <32556354+AlanGarc1a@users.noreply.github.com>
2021-01-30 11:01:23 -0800

    removed each individual style and put them all in one style file. added ejs-mate

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const path    = require('path');
+const ejsMate = require('ejs-mate');
 
 const app = express();
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
-
+app.engine('ejs', ejsMate);
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
